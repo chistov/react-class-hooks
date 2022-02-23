7c48b1cd79216aa2c62b2fb621299dadb30ab60e
Sergey Chistov <s.chistov@globus-ltd.com>
2022-02-23 19:37:17 +0300

    2nd test added

--- src/ChildWithBtn.spec.ts
+++ src/ChildWithBtn.spec.ts
@@ -1,9 +1,9 @@
 import ChildWithBtn from "./ChildWithBtn";
+import {create} from 'react-test-renderer';
 import App from './index';
 
 describe('test component functionality', () => {
-  test('check initial Item state', () => {
-
+  test('check initial event subscription', () => {
     const child = new ChildWithBtn({})
     const spy = jest.spyOn(child, 'srvReq');
     spy.mockReturnValue(null);
@@ -11,5 +11,15 @@ describe('test component functionality', () => {
     child.componentDidMount();
 
     expect(window.addEventListener).toHaveBeenCalled()
+  });
+
+  test('handle key down', () => {
+    const child = new ChildWithBtn({})
+    const spy = jest.spyOn(child, 'srvReq');
+    spy.mockReturnValue(null);
+    console.log = jest.fn(() => {});
+    child.handleKeyDown(new KeyboardEvent('down'));
+
+    expect(console.log).toHaveBeenCalled()
   });
 });
