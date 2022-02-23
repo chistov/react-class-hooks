3514010cf335961bdb06fdf1b52f589dd5c84732
Sergey Chistov <s.chistov@globus-ltd.com>
2022-02-23 15:33:29 +0300

    extra code removed

--- src/ChildWithBtn.spec.ts
+++ src/ChildWithBtn.spec.ts
@@ -1,11 +1,5 @@
-import * as React from "react";
-import renderer from 'react-test-renderer';
-import userEvent from '@testing-library/user-event';
-import {render, screen} from '@testing-library/react'
 import ChildWithBtn from "./ChildWithBtn";
 import App from './index';
-const ReactTestRenderer = require('react-test-renderer');
-
 
 describe('test component functionality', () => {
   test('check initial Item state', () => {
